c12b6b9eb44f76eba13b15a3972a30dd1b341edb
sucik <sucik@outlook.com>
2020-10-27 00:13:48 +0000

    added python tule file

--- python/python_list.py
+++ python/python_list.py
@@ -82,3 +82,14 @@ print(f'this is the list sorted: {linux_distroes}')
 # reversing the items in a list
 linux_distroes.reverse()
 print(linux_distroes)
+
+
+# python multidimensional lists
+number_sets = [[2, 4, 6, 8, 10], [3, 6, 9, 12, 15], [4, 8, 12, 16, 20]]
+print(number_sets[1])
+print(number_sets[1][1])
+
+
+number_sets = [[[1, 2, 3, 4],[5, 6, 7, 8,],[9, 10, 11, 12],], [[13, 14, 15, 16],[17, 18, 19, 20],[21, 22, 23, 24]], [[25, 26, 27, 28], [29, 30, 31, 32], [33, 34, 35, 36]]]
+print(number_sets[0][1][2])
+
---
+++ python/python_tuples.py
@@ -0,0 +1,8 @@
+# tuples are immutable data type which means you can not chnaged once you create it
+
+random_junk = ('bacon', 5, True, 11, 'your mother was a hamster!',)
+print(len(random_junk))
+print(random_junk[len(random_junk) -1])
+
+
+
